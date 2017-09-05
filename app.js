2ce1b09d92f1c76e63bb39be73f483ff24c45da8
Marty <marty.stepien@gmail.com>
2017-09-05 09:05:21 +0100

    second page added

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var app = express();
 // Routes
 
 // home
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send("This is a server response on the home page");
 });
 
-app.listen(3000, function() {
+// movie_single
+app.get('/london_brutalism_icon/:icon_number?', function (req, res) {
+    var icon_number = req.params.icon_numer;
+    res.send("This is a page for brutalism icon " + icon_number);
+});
+
+app.listen(3000, function () {
     console.log('This application is running on local host:3000');
 });
